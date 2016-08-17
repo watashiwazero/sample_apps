8b43ced55ee27a0dce7b86577f788de4a1679f89
Arnaez <mark.a.f.arnaez@accenture.com>
2016-08-17 19:05:36 +0800

    sample ant project

---
+++ ant/passion4java/src/com/passion4java/helloworld/ant/AntHelloWorld.java
@@ -0,0 +1,9 @@
+package com.passion4java.helloworld.ant;
+
+public class AntHelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("This Project was build using Ant");
+	}
+
+}
